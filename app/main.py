a4b3608a95898ee0aeb7aa1f6528206072d3eb79
Juan Equihua <juanequihua@juans-mbp.home>
2023-01-15 18:22:49 +0000

    test

--- app/main.py
+++ app/main.py
@@ -28,6 +28,6 @@ app.include_router(vote.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my API!!!"}
+    return {"message": "Welcome to my API!!! Now deployed.!"}
 
 
